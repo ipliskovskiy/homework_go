b26face2d7a3a0b40b3e1f180e391d9b429f3d71
Илья Плисковский <i.pliskovskiy@city-mobil.ru>
2022-03-08 18:12:38 +0300

    Delete prefix

--- internal/tools/stringWorker/coordinate.go
+++ internal/tools/stringWorker/coordinate.go
@@ -4,11 +4,11 @@ import (
 	"errors"
 )
 
-var BadCoordinates error = errors.New("bad coordinates! error: no correct string")
+var ErrBadCoordinates error = errors.New("bad coordinates! error: no correct string")
 
 func GetCoordinate(coordinates string) (uint8, uint8, error) {
 	if len(coordinates) != 3 {
-		return 0, 0, errors.New("bad coordinates! error: no correct string")
+		return 0, 0, ErrBadCoordinates
 	}
 	return uint8(coordinates[0] - 48), uint8(coordinates[2] - 48), nil //не нашел как конвертировать byte в int без 48
 }
