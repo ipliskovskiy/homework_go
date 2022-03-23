6620747210feedf5bed7ad2445cc38ec799f9c8e
Илья Плисковский <i.pliskovskiy@city-mobil.ru>
2022-03-23 19:30:39 +0300

    add server

--- internal/webui/server/ttserver.go
+++ internal/webui/server/ttserver.go
@@ -14,5 +14,10 @@ func SimpleServer() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!")
 	})
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "test")
+	})
 	http.ListenAndServe(":80", nil)
 }
+
+//https://tproger.ru/translations/go-web-server/
