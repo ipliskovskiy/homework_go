af024efe72946a6ec51a4181c0352e1a9fa29dfe
Илья Плисковский <i.pliskovskiy@gmail.com>
2022-05-23 22:50:46 +0300

    add handlerIndex

--- internal/web/server.go
+++ internal/web/server.go
@@ -4,32 +4,40 @@ import (
 	"fmt"
 	"net/http"
 	"html/template"
-	"os"
+	// "gb_golang/internal/tools/stringWorker"
 )
 
-type StartInfo struct{
+type IndexPage struct{
 	Title string
 	Message string
 	BottomInformation string
+	Player1 string
+	Player2 string
+}
+
+var (
+	tmpl = template.Must(template.ParseFiles("internal/web/templates/index.html"))
+)
+
+func handlerIndex(w http.ResponseWriter, r *http.Request){
+	data := IndexPage {
+		Title: "TicTacToe",
+		Message: "Добро пожаловать в увлекательную интерактивную игру крестики нолики.",
+		BottomInformation: "Version: Experemental",
+		Player1: r.FormValue("player1"),
+		Player2: r.FormValue("player2"),
+	}
+	tmpl.Execute(w, data)
 }
 
 func StartServer() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		data := StartInfo {
-			Title: "TicTacToe",
-			Message: "Добро пожаловать в увлекательную интерактивную игру крестики нолики.",
-			BottomInformation: "Version: Experemental",
-		}
-		tmpl, _ := template.ParseFiles("internal/web/templates/index.html")
-        tmpl.Execute(w, data)
-	})
-
+	http.HandleFunc("/", handlerIndex)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("internal/web/templates/css"))))
 	http.HandleFunc("/hello", sayhello)
 	fmt.Println("Start server...")
 	http.ListenAndServe(":8080", nil)
+	http.
 }
-
 
 func sayhello(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Привет!")
